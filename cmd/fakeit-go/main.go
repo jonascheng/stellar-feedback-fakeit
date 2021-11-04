166962f3e2551a53a38dd65a9eda35bd9dfe2dc8
Jonas Cheng <jonas_cheng@trendmicro.com>
2021-11-04 19:37:34 +0800

    feat(wip): added system env

--- cmd/fakeit-go/main.go
+++ cmd/fakeit-go/main.go
@@ -3,104 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
-	"time"
 
 	"github.com/brianvoe/gofakeit/v6"
 
 	"github.com/jonascheng/stellar-feedback-fakeit/internal/myfakeit"
 )
 
-// Create structs with random injected data
-type Foo struct {
-	Str           string
-	Friendname    string `fake:"{friendname}"`
-	Int           int
-	Pointer       *int
-	Name          string         `fake:"{firstname}"`  // Any available function all lowercase
-	Sentence      string         `fake:"{sentence:3}"` // Can call with parameters
-	RandStr       string         `fake:"{randomstring:[hello,world]}"`
-	Number        string         `fake:"{number:1,10}"`       // Comma separated for multiple values
-	Regex         string         `fake:"{regex:[abcdef]{5}}"` // Generate string from regex
-	Map           map[string]int `fakesize:"2"`
-	Array         []string       `fakesize:"2"`
-	Bar           Bar
-	Skip          *string               `fake:"skip"` // Set to "skip" to not generate data for
-	Created       time.Time             // Can take in a fake tag as well as a format tag
-	CreatedFormat time.Time             `fake:"{year}-{month}-{day}" format:"2006-01-02"`
-	Address       *gofakeit.AddressInfo `fake:"{address}"`
-	System        *myfakeit.SystemInfo  `fake:"{system}"`
+type AgentSystemEnv struct {
+	SystemEnv myfakeit.SystemEnv `json:"system" xml:"system" fake:"{system}"`
+	// AddressInfo *gofakeit.AddressInfo `fake:"{address}"`
+	// PersonInfo  *gofakeit.PersonInfo  `fake:"{person}"`
+	// UUID string `fake:"{uuid}"`
+	// CreatedFormat time.Time `format:"MM/dd/yyyy"`
 }
 
-type Bar struct {
-	Name   string
-	Number int
-	Float  float32
-}
-
-type Info struct {
-	System      myfakeit.SystemInfo   `fake:"{system}"`
-	AddressInfo *gofakeit.AddressInfo `fake:"{address}"`
-	PersonInfo  *gofakeit.PersonInfo  `fake:"{person}"`
-	UUID        string                `fake:"{uuid}"`
-}
-
-type Bulk struct {
-	Data []Info `fakesize:"1"`
+type BulkAgentSystemEnv struct {
+	Agents []AgentSystemEnv `json:"agents" xml:"agents" fakesize:"3"`
 }
 
 func testStruct() {
-	// Pass your struct as a pointer
-	var f Foo
-	gofakeit.Struct(&f)
-
-	fmt.Println(f.Str)              // hrukpttuezptneuvunh
-	fmt.Println(f.Friendname)       // hrukpttuezptneuvunh
-	fmt.Println(f.Int)              // -7825289004089916589
-	fmt.Println(*f.Pointer)         // -343806609094473732
-	fmt.Println(f.Name)             // fred
-	fmt.Println(f.Sentence)         // Record river mind.
-	fmt.Println(f.RandStr)          // world
-	fmt.Println(f.Number)           // 4
-	fmt.Println(f.Regex)            // cbdfc
-	fmt.Println(f.Map)              // map[PxLIo:52 lxwnqhqc:846]
-	fmt.Println(f.Array)            // cbdfc
-	fmt.Printf("%+v", f.Bar)        // {Name:QFpZ Number:-2882647639396178786 Float:1.7636692e+37}
-	fmt.Println(f.Skip)             // <nil>
-	fmt.Println(f.Created.String()) // 1908-12-07 04:14:25.685339029 +0000 UTC
-	fmt.Println(f.Address)
-	fmt.Println(f.System)
-
-	var b Bulk
-	gofakeit.Struct(&b)
-	x, err := json.Marshal(b)
+	var agents BulkAgentSystemEnv
+	gofakeit.Struct(&agents)
+	x, err := json.Marshal(agents)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(x))
-
 }
 
 func main() {
-	fmt.Println(gofakeit.FuncLookups["system"])
-
-	fmt.Println(gofakeit.Name())
-
-	fmt.Println(gofakeit.Address())
-
-	gofakeit.AddFuncLookup("friendname", gofakeit.Info{
-		Category:    "custom",
-		Description: "Random friend name",
-		Example:     "bill",
-		Output:      "string",
-		Generate: func(r *rand.Rand, m *gofakeit.MapParams, info *gofakeit.Info) (interface{}, error) {
-			return gofakeit.RandomString([]string{"bill", "bob", "sally"}), nil
-		},
-	})
-
-	fmt.Println("===")
 	testStruct()
-
-	fmt.Println("===")
-	fmt.Println(myfakeit.System().OSInfo)
 }
--- internal/myfakeit/system-env.go
+++ internal/myfakeit/system-env.go
@@ -2,42 +2,43 @@ package myfakeit
 
 import (
 	"math/rand"
+	"time"
 
 	"github.com/brianvoe/gofakeit/v6"
 )
 
-type SystemInfo struct {
-	Guid   string               `json:"guid" xml:"guid"`
-	OSInfo *OperatingSystemInfo `json:"os" xml:"os"`
+type SystemEnv struct {
+	Guid    string       `json:"guid" xml:"guid" fake:"{uuid}"`
+	Caption string       `json:"caption" xml:"caption"`
+	Version string       `json:"version" xml:"version"`
+	Qfe     []QfeInfo    `json:"qfe" xml:"qfe" fakesize:"5"`
+	Volume  []VolumeInfo `json:"volume" xml:"volume" fakesize:"2"`
+	// Meta    []MetaInfo   `json:"meta" xml:"meta"`
 }
 
-type OperatingSystemInfo struct {
-	Caption string `json:"caption" xml:"caption"`
-	Version string `json:"version" xml:"version"`
+type QfeInfo struct {
+	HotfixId    string    `json:"hotfixid" xml:"hotfixid" fake:"{hotfixid}"`
+	InstalledOn time.Time `json:"installedon" xml:"installedon" format:"MM/dd/yyyy"`
 }
+
+type VolumeInfo struct {
+	Total string `json:"total" xml:"total" fake:"{number:2000000000000,4000000000000}"`
+	Free  string `json:"free" xml:"free" fake:"{number:2000000000,4000000000}"`
+	Type  string `json:"type" xml:"type" fake:"{randomstring:[FAT32,NTFS]}"`
+}
+
+// type MetaInfo struct {
+// }
 
 // System will generate a struct of system information
-func System() *SystemInfo { return system(globalFaker.Rand) }
+func System() *SystemEnv { return system(globalFaker.Rand) }
 
-func system(r *rand.Rand) *SystemInfo {
-	guid := gofakeit.UUID()
-	os := operatingSystem(r)
-
-	return &SystemInfo{
-		Guid:   guid,
-		OSInfo: os,
-	}
-}
-
-// OperatingSystem will generate a struct of operating system information
-func OperatingSystem() *OperatingSystemInfo { return operatingSystem(globalFaker.Rand) }
-
-func operatingSystem(r *rand.Rand) *OperatingSystemInfo {
-	caption := operatingSystemCaption(r)
-
-	return &OperatingSystemInfo{
-		Caption: caption,
-	}
+func system(r *rand.Rand) *SystemEnv {
+	var s SystemEnv
+	gofakeit.Struct(&s)
+	s.Caption = operatingSystemCaption(r)
+	s.Version = operatingSystemVersion(r)
+	return &s
 }
 
 // OperatingSystemCaption will generate a random operating system string
@@ -47,15 +48,38 @@ func operatingSystemCaption(r *rand.Rand) string {
 	return getRandValue(r, []string{"OperatingSystem", "caption"})
 }
 
+// OperatingSystemVersion will generate a random operating system version string
+func OperatingSystemVersion() string { return operatingSystemVersion(globalFaker.Rand) }
+
+func operatingSystemVersion(r *rand.Rand) string {
+	return getRandValue(r, []string{"OperatingSystem", "version"})
+}
+
+// OperatingSystemQfe will generate a random operating system qfe
+func OperatingSystemQfe() string { return operatingSystemQfe(globalFaker.Rand) }
+
+func operatingSystemQfe(r *rand.Rand) string {
+	return "KB" + gofakeit.DigitN(7)
+}
+
 func addSystemLookup() {
 	gofakeit.AddFuncLookup("system", gofakeit.Info{
 		Display:     "System",
 		Category:    "system",
 		Description: "Random set of system information",
-		Example:     "TODO",
-		Output:      "TODO",
 		Generate: func(r *rand.Rand, m *gofakeit.MapParams, info *gofakeit.Info) (interface{}, error) {
 			return system(r), nil
+		},
+	})
+
+	gofakeit.AddFuncLookup("hotfixid", gofakeit.Info{
+		Display:     "HotfixID",
+		Category:    "hotfixid",
+		Description: "Random hotfix ID",
+		Example:     "KB5004331",
+		Output:      "string",
+		Generate: func(r *rand.Rand, m *gofakeit.MapParams, info *gofakeit.Info) (interface{}, error) {
+			return operatingSystemQfe(r), nil
 		},
 	})
 }
