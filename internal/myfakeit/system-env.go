8a408ea9683d666f2b2b3bc345e95225ede9b7ea
Jonas Cheng <jonas_cheng@trendmicro.com>
2021-11-05 10:11:46 +0800

    feat(system): implemented system env fakeit

--- internal/myfakeit/system-env.go
+++ internal/myfakeit/system-env.go
@@ -2,23 +2,24 @@ package myfakeit
 
 import (
 	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/brianvoe/gofakeit/v6"
 )
 
 type SystemInfo struct {
-	Guid    string       `json:"guid" xml:"guid" fake:"{uuid}"`
-	Caption string       `json:"caption" xml:"caption" fake:"{os-caption}"`
-	Version string       `json:"version" xml:"version" fake:"{os-version}"`
-	Qfe     []QfeInfo    `json:"qfe" xml:"qfe" fakesize:"5"`
-	Volume  []VolumeInfo `json:"volume" xml:"volume" fakesize:"2"`
-	// Meta    []MetaInfo   `json:"meta" xml:"meta"`
+	Guid    string              `json:"guid" xml:"guid" fake:"{uuid}"`
+	Caption string              `json:"caption" xml:"caption" fake:"{os-caption}"`
+	Version string              `json:"version" xml:"version" fake:"{os-version}"`
+	Qfe     []QfeInfo           `json:"qfe" xml:"qfe" fakesize:"5"`
+	Volume  []VolumeInfo        `json:"volume" xml:"volume" fakesize:"2"`
+	Meta    []map[string]string `json:"meta" xml:"meta"`
 }
 
 type QfeInfo struct {
-	HotfixId    string    `json:"hotfixid" xml:"hotfixid" fake:"{hotfixid}"`
-	InstalledOn time.Time `json:"installedon" xml:"installedon" format:"MM/dd/yyyy"`
+	HotfixId    string `json:"hotfixid" xml:"hotfixid" fake:"{qfe-hotfixid}"`
+	InstalledOn string `json:"installedon" xml:"installedon" fake:"{qfe-installedon}"`
 }
 
 type VolumeInfo struct {
@@ -27,15 +28,13 @@ type VolumeInfo struct {
 	Type  string `json:"type" xml:"type" fake:"{randomstring:[FAT32,NTFS]}"`
 }
 
-// type MetaInfo struct {
-// }
-
 // System will generate a struct of system information
 func SystemEnv() *SystemInfo { return systemEnv(globalFaker.Rand) }
 
 func systemEnv(r *rand.Rand) *SystemInfo {
 	var s SystemInfo
 	err := gofakeit.Struct(&s)
+	s.Meta = operatingSystemMeta(r)
 	if err != nil {
 		panic(err)
 	}
@@ -61,6 +60,42 @@ func OperatingSystemQfe() string { return operatingSystemQfe(globalFaker.Rand) }
 
 func operatingSystemQfe(r *rand.Rand) string {
 	return "KB" + gofakeit.DigitN(7)
+}
+
+// OperatingSystemQfeInstalledOn will generate a random operating system qfe
+func OperatingSystemQfeInstalledOn() string { return operatingSystemQfeInstalledOn(globalFaker.Rand) }
+
+func operatingSystemQfeInstalledOn(r *rand.Rand) string {
+	// Declaring layout constant
+	const layout = "02/01/2006"
+
+	startdate, err := time.Parse(layout, "01/01/2021")
+	if err != nil {
+		panic(err)
+	}
+	enddate, _ := time.Parse(layout, "31/12/2021")
+	if err != nil {
+		panic(err)
+	}
+	installon := gofakeit.DateRange(startdate, enddate)
+	return installon.Format(layout)
+}
+
+// OperatingSystemMeta will generate a random operating system meta
+func OperatingSystemMeta() []map[string]string { return operatingSystemMeta(globalFaker.Rand) }
+
+func operatingSystemMeta(r *rand.Rand) []map[string]string {
+	var list []map[string]string
+	list = append(list, map[string]string{
+		"CpuCaption":      "Intel64 Family 6 Model 167 Stepping 1",
+		"CpuName":         "11th Gen Intel(R) Core(TM) i7-11700 @ 2.50GHz",
+		"CpuArchitecture": strconv.Itoa(gofakeit.RandomInt([]int{0, 5, 9, 12})),
+		"OsArchitecture":  gofakeit.RandomString([]string{"64-bit", "32-bit"}),
+		"OSLanguage":      gofakeit.RandomString([]string{"1033", "1028", "1041"}),
+		"TotalMemory":     strconv.Itoa(gofakeit.Number(30000000000, 40000000000)),
+		"FreeMemory":      strconv.Itoa(gofakeit.Number(10000000, 20000000)),
+	})
+	return list
 }
 
 func addSystemLookup() {
@@ -95,14 +130,25 @@ func addSystemLookup() {
 		},
 	})
 
-	gofakeit.AddFuncLookup("hotfixid", gofakeit.Info{
+	gofakeit.AddFuncLookup("qfe-hotfixid", gofakeit.Info{
 		Display:     "Hotfix ID",
-		Category:    "hotfixid",
+		Category:    "qfe-hotfixid",
 		Description: "Random hotfix ID",
 		Example:     "KB5004331",
 		Output:      "string",
 		Generate: func(r *rand.Rand, m *gofakeit.MapParams, info *gofakeit.Info) (interface{}, error) {
 			return operatingSystemQfe(r), nil
+		},
+	})
+
+	gofakeit.AddFuncLookup("qfe-installedon", gofakeit.Info{
+		Display:     "Hotfix Install On",
+		Category:    "qfe-installedon",
+		Description: "Random hotfix Install On",
+		Example:     "8/10/2021",
+		Output:      "string",
+		Generate: func(r *rand.Rand, m *gofakeit.MapParams, info *gofakeit.Info) (interface{}, error) {
+			return operatingSystemQfeInstalledOn(r), nil
 		},
 	})
 }
