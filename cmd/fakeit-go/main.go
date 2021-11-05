1cb20fc66d8ab6cd1639c3d6cbaacf9a34ae2df3
Jonas <jonas.cheng+trello@gmail.com>
2021-11-05 13:46:34 +0800

    feat(main): added command line handler

--- cmd/fakeit-go/main.go
+++ cmd/fakeit-go/main.go
@@ -5,9 +5,21 @@ import (
 	"fmt"
 
 	"github.com/jonascheng/stellar-feedback-fakeit/internal/builder"
+	// A Go (golang) command line and flag parser
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+var (
+	showList   = kingpin.Flag("list", "Show available speedtest.net servers.").Short('l').Bool()
+	serverIds  = kingpin.Flag("server", "Select server id to speedtest.").Short('s').Ints()
+	savingMode = kingpin.Flag("saving-mode", "Using less memory (≒10MB), though low accuracy (especially > 30Mbps).").Bool()
+	jsonOutput = kingpin.Flag("json", "Output results in json format").Bool()
 )
 
 func main() {
+	kingpin.Version("1.0.0")
+	kingpin.Parse()
+
 	agents := builder.CollectAgentSystemEnv(1)
 	jsonBytes, err := json.Marshal(agents)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/jonascheng/stellar-feedback-fakeit
 
 go 1.16
 
-require github.com/brianvoe/gofakeit/v6 v6.9.0
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/brianvoe/gofakeit/v6 v6.9.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
