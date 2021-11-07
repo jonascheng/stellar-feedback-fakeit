0483bab307fe8e46530a0751adb23aeac336b6e8
Jonas <jonas.cheng+trello@gmail.com>
2021-11-07 12:34:26 +0800

    docs(system): added comments

--- internal/data/system-env.go
+++ internal/data/system-env.go
@@ -2,6 +2,8 @@ package data
 
 // Address consists of address information
 var OperatingSystem = map[string][]string{
+	// https://en.wikipedia.org/wiki/List_of_Microsoft_Windows_versions
 	"caption": {"Microsoft Windows 10 Enterprise", "Microsoft Windows 10 Pro for Workstations", "Microsoft Windows 10 Pro"},
 	"version": {"10.0.19042", "10.0.19041", "10.0.18363", "10.0.18362", "10.0.17763", "10.0.17134", "10.0.16299", "10.0.15063", "10.0.14393", "10.0.10586", "10.0.10240"},
+	//
 }
